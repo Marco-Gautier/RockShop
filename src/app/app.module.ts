677c41b3ecf358339343d52203050011f40414ec
Marco Gautier <marco.gautier@epitech.eu>
2019-04-05 01:18:54 +0200

    feat(routing): Add routing and a new blog route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes : Routes = [
+  { path: 'blog', component: BlogComponent },
+  { path: '', component: BlogComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
